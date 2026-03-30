eafd64769e815393c273f41ddd0de619d7c21ded
Matthias Lehmann <matthias.lehmann@aviotech.de>
2026-03-30 13:35:25 +0200

    add github name

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://yourusername.github.io',
+  site: 'https://MatthiasLehmann.github.io',
   base: '/req_man',
 });
